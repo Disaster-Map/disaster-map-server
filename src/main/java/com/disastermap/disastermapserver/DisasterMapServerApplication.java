2f764f316072811ef60f681918778b1d36492552
신현섭 <didakd22@naver.com>
2025-08-01 08:40:49 +0900

    feat : Application 클래스에 스케줄러 어노테이션 추가

--- src/main/java/com/disastermap/disastermapserver/DisasterMapServerApplication.java
+++ src/main/java/com/disastermap/disastermapserver/DisasterMapServerApplication.java
@@ -2,7 +2,9 @@ package com.disastermap.disastermapserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class DisasterMapServerApplication {
 
